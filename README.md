0c5dd83dfddb83af32830d6f2fc87f8b428fa599
vishwanath1306 <vishwa.nath@outlook.com>
2023-02-06 17:41:31 -0700

    Added ignore, readme and started buffer

---
+++ README.md
@@ -0,0 +1,11 @@
+# Parsing Hindsight Traces
+
+This is a parser for the collector output from hindsight, a new tracing system. 
+
+## Usage
+
+Compiling: 
+
+```
+g++ parse_trace.cc -ltracer -o parse_trace.o
+```
--- parse_trace.cc
+++ parse_trace.cc
@@ -11,8 +11,17 @@ Program to read the output of hindsight traces. Partly adpated from the process.
 #include <fstream>
 #include <sstream>
 
+extern "C" {
+    #include "tracer/tracestate.h"
+}
+
+// ============ START ARGUMENTS ===================
+
 static char doc[] = "For parsing the hindsight collector output";
 static char args_doc[] = "FILENAME";
+
+bool DEBUG = false;
+bool WARN = false; 
 
 static struct argp_option options [] = {
     {"debug", 'd', 0, 0, "Print debug information"},
@@ -55,7 +64,30 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state){
 
 static struct argp argp = {options, parse_opt, args_doc, doc};
 
+// ============ END ARGUMENTS ===================
 
+// ============ START BUFFERS ===================
+
+class RawHindsightBuffer {
+    public: 
+        std::string agent; 
+        TraceHeader* header;
+
+};
+
+void read_buffers(std::string filename){
+    std::fstream dataset_fd(filename, std::ios_base::in);
+
+    dataset_fd.seekg(0, std::ios::end);
+    size_t length = dataset_fd.tellg();
+    dataset_fd.seekg(0, std::ios::beg);
+
+    if(DEBUG){
+        std::cout << filename << " has length " << length << std::endl;
+    }
+
+
+}
 
 int main(int argc, char**argv){
 
@@ -65,7 +97,11 @@ int main(int argc, char**argv){
 
     argp_parse(&argp, argc, argv, 0, 0, &args);
 
+    DEBUG = args.debug;
+    WARN = args.warn;
+
     std::cout<<"input file is: "<<args.ip_file<<std::endl;
 
+    read_buffers(args.ip_file);
     return 1;
 }
