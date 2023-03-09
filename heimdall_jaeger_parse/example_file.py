2f99611bf7f197e5bfd1d297b94b4e8b0788436c
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2023-03-09 09:29:34 -0500

    Added trace file

--- heimdall_jaeger_parse/example_file.py
+++ heimdall_jaeger_parse/example_file.py
@@ -1,6 +1,6 @@
 import heimdall_jaeger_parse
 
-val3 = heimdall_jaeger_parse.JRoot(filename="/Users/vishwanath/Developer/Rust/trace-graph-store/base_storage/example_json/example_jaeger_trace.json")
+val3 = heimdall_jaeger_parse.JRoot(filename="../jaeger_traces.json")
 print(val3.data[0].trace_id)
 for span in val3.data[0].spans:
     if len(span.references) != 0:
