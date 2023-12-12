2f6af3f6cf3933f0ef1fec8782d2b233ed4f03ec
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2023-12-12 13:29:04 -0500

    Added scene graph conversion start

--- parse_jaeger.py
+++ parse_jaeger.py
@@ -1,13 +1,55 @@
 import json
+from typing import Dict
 
 
-def read_trace(filename):
+def read_trace(file_name: str) -> Dict:
+    return json.load(open(filename, 'r'))
+
+
+'''
+"2407890": {
+        "width": 640,
+        "height": 480,
+        "location": "living room",
+        "weather": none,
+        "objects": {
+            "271881": {
+                "name": "chair",
+                "x": 220,
+                "y": 310,
+                "w": 50,
+                "h": 80,
+                "attributes": ["brown", "wooden", "small"],
+                "relations": {
+                    "32452": {
+                        "name": "on",
+                        "object": "275312"
+                    },
+                    "32452": {
+                        "name": "near",
+                        "object": "279472"
+                    }                    
+                }
+            }
+        }
+    }
     
-    whole_trace = json.load(open(filename, 'r'))
-    data = whole_trace['data']
-    print(len(data))
+'''
+def convert_to_scene_graph(individual_trace):
+    scene_graph_dict = {}
+    scene_graph_dict[individual_trace['traceID']] = {}
+    scene_graph_dict[individual_trace['traceID']]['location'] = None
+    scene_graph_dict[individual_trace['traceID']]['objects'] = {}
+
+    current_spans = individual_trace['spans']
+    for span in current_spans:
+        if span['traceID'] == span['spanID']:
+            scene_graph_dict[individual_trace['traceID']]['location'] = span['operationName']
+    print(scene_graph_dict)
 
 
 if __name__ == "__main__":
     filename = 'jaeger_traces.json'
-    read_trace(filename=filename)
+    jaeger_traces = read_trace(file_name=filename)
+    for trace in jaeger_traces['data']:
+        convert_to_scene_graph(trace)
